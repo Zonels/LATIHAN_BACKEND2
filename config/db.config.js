cad6de6fd7f39834d3f1d774f928333c89087725
Zonels <okta.dwi.1990@gmail.com>
2026-06-03 11:04:46 +0700

    Adding some config

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,17 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2";
-const db = new Sequelize('web_lanjut', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectModule: mysql,
-    "define": {
-        "timestamps": false
-    }
+const db = new Sequelize("web_lanjut", "avnadmin", "AVNS_snLs5suqA5X2bEuut2-", {
+  host: "mysql-1c2b2c05-latihan.h.aivencloud.com",
+  dialect: "mysql",
+  port: 25151,
+  dialectOptions: { ssl: { rejectUnauthorized: false } },
+  define: { timestamps: false },
 });
 export default db;
 
 (async () =>{
     await db.sync();
 })();
+
+
+
